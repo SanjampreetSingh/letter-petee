4028de2c8d9e84cff8a13deef26f204afa14e011
Sanjampreet Singh <sanjam531@gmail.com>
2024-01-28 12:20:11 +0530

    feat: added navbar

---
+++ src/components/custom/navbar/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="left-0 top-0 flex h-[24px] w-full flex-1 items-center justify-center p-8">
+      Letter Petee
+    </nav>
+  );
+}
