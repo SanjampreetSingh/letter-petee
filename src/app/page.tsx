7eac1f753009c9746aad436a10b54cc5f2a503ce
Sanjampreet Singh <sanjam531@gmail.com>
2024-01-28 21:00:36 +0530

    minimal fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Playground from "@/components/custom/playground/playground";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-8">
-      <Playground />
-    </main>
-  );
+  return <Playground />;
 }
--- src/components/custom/playground/playground.tsx
+++ src/components/custom/playground/playground.tsx
@@ -8,14 +8,16 @@ export default function Playground() {
   const [letter, setLetter] = useState<string>("");
 
   return (
-    <div className="flex h-[240px] flex-row gap-10">
-      <Textarea onChange={(e) => setLetter(e.target.value)} />
-      <>
-        <Separator orientation="vertical" className="h-full">
-          {" "}
-        </Separator>
-      </>
-      <div>{letter}</div>
-    </div>
+    <main className="flex h-[70vh] flex-col gap-8 px-8 md:flex-row">
+      <Textarea
+        onChange={(e) => setLetter(e.target.value)}
+        className="flex-1 resize-none rounded-lg border border-gray-300 bg-white p-4 shadow-lg"
+      />
+
+      <Separator orientation="vertical" className="h-full"></Separator>
+      <div className="flex-1 overflow-auto whitespace-pre-wrap break-all rounded-lg border border-gray-300 bg-white p-4 shadow-lg">
+        {letter}
+      </div>
+    </main>
   );
 }
